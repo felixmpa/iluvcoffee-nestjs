3b77b2da00ca23297ba0dbcd24f225bf840e7cc0
Felix <felixmpa@gmail.com>
2024-05-28 19:03:21 -0400

    added relations

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -12,24 +12,24 @@ import {
   Res,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
-import { Coffee } from "./entities/coffee.entity";
-import { CreateCoffeeDto } from "./dto/create-coffee.dto";
-import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import { Coffee } from './entities/coffee.entity';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
   @Get()
-  findAll(@Res() response, @Query() paginationQuery) {
+  async findAll(@Res() response, @Query() paginationQuery) {
     //const { limit, offset } = paginationQuery;
     //const msg: string = `this action return all coffees. Limit: ${limit} and Offset: ${offset}`;
-    const allCoffees: Coffee[] = this.coffeesService.findAll();
+    const allCoffees: Coffee[] = await this.coffeesService.findAll();
     return response.status(HttpStatus.OK).send(allCoffees);
   }
 
   @Get(':id')
-  findOne(@Res() response, @Param('id') id: string) {
-    const coffee = this.coffeesService.findOne(id);
+  async findOne(@Res() response, @Param('id') id: string) {
+    const coffee = await this.coffeesService.findOne(id);
     return response.status(HttpStatus.OK).send(coffee);
   }
 
@@ -40,8 +40,11 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-    return this.coffeesService.update(id, updateCoffeeDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateCoffeeDto: UpdateCoffeeDto,
+  ) {
+    return await this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
